8bc922c1bf745ae84023a08d1e88609031dec6a8
DavidVazquez <burnkillreach@gmail.com>
2017-03-26 15:01:53 -0600

    Agregado PNG de bd v6

--- db/data.sql
+++ db/data.sql
@@ -17,11 +17,20 @@ INSERT INTO `categoria` (`idCategoria`,`nombre`, `iniciales`) VALUES
 -- Volcado de datos para la tabla `producto`
 --
 
-INSERT INTO `producto` (`nombre`, `Categoria_idCategoria`,`iniciales`) VALUES
-('TAPETE 1X1M ROSA AFELPADO', 2,`TAP-ROS-1X1`),
-('TAPETE 1X1M VERDE AFELPADO', 2,`TAP-VRD-1X1`),
-('TAPETE 2X1M AZUL', 2,`TAP-AZL-2X1`),
-('EPSON 20 LUMENES 20 WATTS FULL HD', 1,`EPSN-20LUM-FHD`),
-('CASIO 15 LUMENES 50 WATTS HD', 1,`CSIO-15LUM-HD`),
+INSERT INTO `producto` (`idProducto`,`nombre`, `Categoria_idCategoria`,`iniciales`) VALUES
+(1,'TAPETE 1X1M ROSA AFELPADO', 2,`TAP-ROS-1X1`),
+(2,'TAPETE 1X1M VERDE AFELPADO', 2,`TAP-VRD-1X1`),
+(3,'TAPETE 2X1M AZUL', 2,`TAP-AZL-2X1`),
+(4,'EPSON 20 LUMENES 20 WATTS FULL HD', 1,`EPSN-20LUM-FHD`),
+(5,'CASIO 15 LUMENES 50 WATTS HD', 1,`CSIO-15LUM-HD`),
+(6,'MESABANCO CAFE CON PALETA RETRACTIL', 3,`MBCO-PLTA`),
+(7,'MESABANCO NEGRO METAL CON PORTAVASOS', 4,`MBCO-PVSOS`);
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `material` (`idMaterial`, `notas`,`Categoria_idCategoria`,`iniciales`) VALUES
+
 ('MESABANCO CAFE CON PALETA RETRACTIL', 3,`MBCO-PLTA`),
 ('MESABANCO NEGRO METAL CON PORTAVASOS', 4,`MBCO-PVSOS`);
