534eb73ad1b82143a21243201151296d2492fe1a
DavidVazquez <burnkillreach@gmail.com>
2017-03-27 18:45:29 -0600

    Agregada barra de progreso a Splash Screen

--- src/ui/Principal.java
+++ src/ui/Principal.java
@@ -4,6 +4,12 @@
 package ui;
 
 import com.alee.laf.WebLookAndFeel;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.SplashScreen;
+import java.awt.geom.Rectangle2D;
 
 /**
  *
@@ -11,9 +17,12 @@ import com.alee.laf.WebLookAndFeel;
  */
 public class Principal extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Principal
-     */
+    static SplashScreen mySplash;
+    static Graphics2D splashGraphics;
+    static Rectangle2D.Double splashTextArea;
+    static Rectangle2D.Double splashProgressArea;
+    static Font font;
+
     public Principal() {
         initComponents();
     }
@@ -83,11 +92,19 @@ public class Principal extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-       
-        WebLookAndFeel.install();
+
+        splashInit();
+        appInit();
+
+        if (mySplash != null) // check if we really had a spash screen
+        {
+            mySplash.close();   // we're done with it
+        }
+
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
+                WebLookAndFeel.install();
                 new Principal().setVisible(true);
             }
         });
@@ -100,4 +117,78 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JToolBar jToolBar1;
     // End of variables declaration//GEN-END:variables
+ private static void splashInit() {
+
+        mySplash = SplashScreen.getSplashScreen();
+
+        if (mySplash != null) {
+            Dimension ssDim = mySplash.getSize();
+            int height = ssDim.height;
+            int width = ssDim.width;
+            splashTextArea = new Rectangle2D.Double(15., height * 0.88, width * .45, 32.);
+            splashProgressArea = new Rectangle2D.Double(width * .55, height * .92, width * .4, 12);
+            splashGraphics = mySplash.createGraphics();
+            font = new Font("Cinzel", Font.PLAIN, 14);
+            splashGraphics.setFont(font);
+            splashText("Starting");
+            splashProgress(0);
+        }
+    }
+
+    private static void appInit() {
+        for (int i = 1; i <= 10; i++) {
+            int pctDone = i * 10;
+            splashText("Doing task #" + i);
+            splashProgress(pctDone);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                break;
+            }
+        }
+    }
+
+    /**
+     * Display text in status area of Splash. Note: no validation it will fit.
+     *
+     * @param str - text to be displayed
+     */
+    public static void splashText(String str) {
+        if (mySplash != null && mySplash.isVisible()) {
+            splashGraphics.setPaint(Color.LIGHT_GRAY);
+            splashGraphics.fill(splashTextArea);
+            splashGraphics.setPaint(Color.BLACK);
+            splashGraphics.drawString(str, (int) (splashTextArea.getX() + 10), (int) (splashTextArea.getY() + 15));
+            mySplash.update();
+        }
+    }
+
+    /**
+     * Display a (very) basic progress bar
+     *
+     * @param pct how much of the progress bar to display 0-100
+     */
+    public static void splashProgress(int pct) {
+        if (mySplash != null && mySplash.isVisible()) {
+
+            splashGraphics.setPaint(Color.WHITE);
+            splashGraphics.fill(splashProgressArea);
+
+            splashGraphics.setPaint(Color.BLUE);
+            splashGraphics.draw(splashProgressArea);
+
+            int x = (int) splashProgressArea.getMinX();
+            int y = (int) splashProgressArea.getMinY();
+            int wid = (int) splashProgressArea.getWidth();
+            int hgt = (int) splashProgressArea.getHeight();
+
+            int doneWidth = Math.round(pct * wid / 100.f);
+            doneWidth = Math.max(0, Math.min(doneWidth, wid - 1));
+
+            splashGraphics.setPaint(Color.GREEN);
+            splashGraphics.fillRect(x, y + 1, doneWidth, hgt - 1);
+
+            mySplash.update();
+        }
+    }
 }
