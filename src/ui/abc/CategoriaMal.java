4af3e89e6ae4d9f5fb1e81173a1cca6e76fa96cb
DavidVazquez <burnkillreach@gmail.com>
2017-03-31 08:02:30 -0600

    Renombrado categoria a CategoriaMal

--- src/ui/abc/CategoriaMal.java
+++ src/ui/abc/CategoriaMal.java
@@ -7,12 +7,12 @@ package ui.abc;
  *
  * @author BurnKill
  */
-public class Categoria extends javax.swing.JFrame {
+public class CategoriaMal extends javax.swing.JFrame {
 
     /**
      * Creates new form Categoria
      */
-    public Categoria() {
+    public CategoriaMal() {
         initComponents();
     }
 
@@ -415,20 +415,21 @@ public class Categoria extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Categoria.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CategoriaMal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Categoria.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CategoriaMal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Categoria.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CategoriaMal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Categoria.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CategoriaMal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Categoria().setVisible(true);
+                new CategoriaMal().setVisible(true);
             }
         });
     }
