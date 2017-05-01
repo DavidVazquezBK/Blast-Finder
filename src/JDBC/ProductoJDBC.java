10bd9386feba57e5d1162017ffac981db90ddc1b
DavidVazquez <burnkillreach@gmail.com>
2017-05-01 16:50:04 -0500

    Agregado ProductoJDBC

---
+++ src/JDBC/ProductoJDBC.java
@@ -0,0 +1,147 @@
+/*
+ * Java Database Connectivity Code Generator v1.0
+ * Author: David Vazquez
+ */
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.table.DefaultTableModel;
+import POJO.ProductoPOJO;
+
+public class ProductoJDBC {
+
+    private static final String TABLE = "Producto";
+
+    private static final String SQL_INSERT = "INSERT INTO " + TABLE + "(nombre, Categoria_idCategoria, iniciales) VALUES (?,?,?)";
+
+    private static final String SQL_QUERY_ALL = "Select * from " + TABLE;
+
+    private static final String SQL_QUERY = "Select * from " + TABLE + " where id=?";
+
+    private static final String SQL_UPDATE = "UPDATE " + TABLE + " set nombre=?, Categoria_idCategoria=?, iniciales=? where idProducto=?";
+    private static final String SQL_DELETE = "Delete from " + TABLE
+            + " where idProducto=?";
+
+    public static int insertaProducto(ProductoPOJO pojo) {
+        Connection con = null;
+        PreparedStatement st = null;
+        try {
+            con = Conexion.getConnection();
+            st = con.prepareStatement(SQL_INSERT);
+            st.setString(1, pojo.getNombre());
+            st.setInt(2, pojo.getCategoria_idCategoria());
+            st.setString(3, pojo.getIniciales());
+            int id = st.executeUpdate();
+            return id;
+        } catch (Exception e) {
+            System.out.println("Error al insertar " + e);
+            return 0;
+        } finally {
+            Conexion.close(con);
+            Conexion.close(st);
+        }
+    }
+
+    public static ProductoPOJO consultar(String id) {
+        Connection con = null;
+        PreparedStatement st = null;
+        ProductoPOJO pojo = new ProductoPOJO();
+        try {
+
+            con = Conexion.getConnection();
+            st = con.prepareStatement(SQL_QUERY);
+            st.setString(1, id);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                pojo.setIdProducto(rs.getInt("idProducto"));
+                pojo.setNombre(rs.getString("nombre"));
+                pojo.setCategoria_idCategoria(rs.getInt("Categoria_idCategoria"));
+                pojo.setIniciales(rs.getString("iniciales"));
+            }
+        } catch (Exception e) {
+            System.out.println("Error al consultar " + e);
+        } finally {
+            Conexion.close(con);
+            Conexion.close(st);
+        }
+        return pojo;
+    }
+
+    public static DefaultTableModel cargarTabla() {
+        Connection con = null;
+        PreparedStatement st = null;
+        String encabezados[] = {"ID", "Nombre", "IDCategoria", "Iniciales"};
+        DefaultTableModel dt = null;
+        try {
+            con = Conexion.getConnection();
+            st = con.prepareStatement(SQL_QUERY);
+            dt = new DefaultTableModel();
+            dt.setColumnIdentifiers(encabezados);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                Object ob[] = new Object[4];
+                ob[0] = rs.getObject("idProducto");
+                ob[1] = rs.getObject("nombre");
+                ob[2] = rs.getObject("Categoria_idCategoria");
+                ob[3] = rs.getObject("iniciales");
+                dt.addRow(ob);
+            }
+            rs.close();
+        } catch (Exception e) {
+            System.out.println("Error al consultar " + e);
+        } finally {
+            Conexion.close(con);
+            Conexion.close(st);
+
+        }
+        return dt;
+    }
+
+    public static boolean eliminarProducto(String id) {
+        Connection con = null;
+        PreparedStatement st = null;
+        try {
+            con = Conexion.getConnection();
+            st = con.prepareStatement(SQL_DELETE);
+            st.setString(1, id);
+            int num = st.executeUpdate();
+            if (num == 0) {
+                return false;
+            }
+        } catch (Exception e) {
+            System.out.println("Error al eliminar = " + e);
+            return false;
+        } finally {
+            Conexion.close(con);
+            Conexion.close(st);
+        }
+        return true;
+    }
+
+    public static boolean actualizaProducto(ProductoPOJO pojo) {
+        Connection con = null;
+        PreparedStatement st = null;
+        try {
+            con = Conexion.getConnection();
+            st = con.prepareStatement(SQL_UPDATE);
+            st.setString(1, pojo.getNombre());
+            st.setInt(2, pojo.getCategoria_idCategoria());
+            st.setString(3, pojo.getIniciales());
+            st.setInt(4, pojo.getIdProducto());
+            int num = st.executeUpdate();
+            if (num == 0) {
+                return false;
+            }
+        } catch (Exception e) {
+            System.out.println("Error al actualizar = " + e);
+            return false;
+        } finally {
+            Conexion.close(con);
+            Conexion.close(st);
+        }
+        return true;
+    }
+
+}
